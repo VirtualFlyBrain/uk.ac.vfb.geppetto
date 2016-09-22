6d5677abf9e27a7e3302c7bcba2cc0e83942aacd
Rob Court <robbie.court@gmail.com>
2016-09-22 10:05:22 +0100

    renaming AddImportTypesImageQueryProcessor

--- src/test/java/uk/ac/vfb/geppetto/test/CrossDataSourceVFBQueryTest.java
+++ src/test/java/uk/ac/vfb/geppetto/test/CrossDataSourceVFBQueryTest.java
@@ -57,7 +57,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 
-import uk.ac.vfb.geppetto.AddImportTypesImageQueryProcessor;
+import uk.ac.vfb.geppetto.AddImportTypesThumbnailQueryProcessor;
 import uk.ac.vfb.geppetto.AddImportTypesQueryProcessor;
 import uk.ac.vfb.geppetto.AddImportTypesRelatedQueryProcessor;
 import uk.ac.vfb.geppetto.AddImportTypesSynonymQueryProcessor;
@@ -96,7 +96,7 @@ public class CrossDataSourceVFBQueryTest
 		context.registerBeanDefinition("vfbImportTypesRelatedQueryProcessor", queryProcessorImportTypesRelatedBeanDefinition);
 		context.registerBeanDefinition("scopedTarget.vfbImportTypesRelatedQueryProcessor", queryProcessorImportTypesRelatedBeanDefinition);
 
-		BeanDefinition queryProcessorImportTypesThumbnailBeanDefinition = new RootBeanDefinition(AddImportTypesImageQueryProcessor.class);
+		BeanDefinition queryProcessorImportTypesThumbnailBeanDefinition = new RootBeanDefinition(AddImportTypesThumbnailQueryProcessor.class);
 		context.registerBeanDefinition("vfbImportTypesThumbnailQueryProcessor", queryProcessorImportTypesThumbnailBeanDefinition);
 		context.registerBeanDefinition("scopedTarget.vfbImportTypesThumbnailQueryProcessor", queryProcessorImportTypesThumbnailBeanDefinition);
 
