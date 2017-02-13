9d50bfb14ad0ba25a924d1be11a2c9cd4cd9e16c
Robert Court <robbie.court@gmail.com>
2017-02-13 18:32:52 +0000

    fix for multiple links

--- src/main/java/uk/ac/vfb/geppetto/AddTypesQueryProcessor.java
+++ src/main/java/uk/ac/vfb/geppetto/AddTypesQueryProcessor.java
@@ -205,7 +205,7 @@ public class AddTypesQueryProcessor extends AQueryProcessor
 	{
 		try
 		{
-			text = text.replaceAll("([F,V,G].*)[:,_](\\d{5}[0-9]*\\b)", "<a href=\"#\" instancepath=\"$1_$2\">$1_$2</a>");
+			text = text.replaceAll("([F,V,G][A-z]*)[:,_](\\d{5}[0-9]*\\b)", "<a href=\"#\" instancepath=\"$1_$2\">$1_$2</a>");
 			return text;
 		}
 		catch(Exception e)
