52fe561ee2d7e8cbd509f05dfff9059749d2e39e
Rob Court <robbie.court@gmail.com>
2017-04-17 11:48:13 +0100

    typo fix

--- src/main/java/uk/ac/vfb/geppetto/AddImportTypesSynonymQueryProcessor.java
+++ src/main/java/uk/ac/vfb/geppetto/AddImportTypesSynonymQueryProcessor.java
@@ -405,7 +405,7 @@ public class AddImportTypesSynonymQueryProcessor extends AQueryProcessor
 	{
 		try
 		{
-			urlString = urlString.relapce('https://','http://');
+			urlString = urlString.replace('https://','http://');
 			URL url = new URL(urlString);
 			HttpURLConnection huc = (HttpURLConnection) url.openConnection();
 			huc.setRequestMethod("HEAD");
