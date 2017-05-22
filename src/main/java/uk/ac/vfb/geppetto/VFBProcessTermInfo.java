e174384d8d52e3e2c5bf3d1237e78024faed2d03
Rob Court <rcourt@ed.ac.uk>
2017-05-22 13:02:12 +0100

    Revert "Swapping to types"
    This reverts commit 74af0c72458bab4bf53246ec4479b19695bb3455.

--- src/main/java/uk/ac/vfb/geppetto/VFBProcessTermInfo.java
+++ src/main/java/uk/ac/vfb/geppetto/VFBProcessTermInfo.java
@@ -467,7 +467,7 @@ public class VFBProcessTermInfo extends AQueryProcessor {
                 // set types:
                 if (types != "") {
                     Variable typesVar = VariablesFactory.eINSTANCE.createVariable();
-                    typesVar.setId("types");
+                    typesVar.setId("type");
                     typesVar.setName("Type");
                     typesVar.getTypes().add(htmlType);
                     metaData.getVariables().add(typesVar);
