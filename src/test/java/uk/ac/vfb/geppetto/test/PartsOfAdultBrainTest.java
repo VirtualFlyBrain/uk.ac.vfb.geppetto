3b9a5506cb5a74e06a7abbba8648a5bf2c5e43c1
Rob Court <rcourt@ed.ac.uk>
2018-06-13 18:08:59 +0100

    Revert "owlery: test case commented, xmi to be fixed"
    This reverts commit ae31d35d81564d0c72ecb69a2792740fea5f3096.

--- src/test/java/uk/ac/vfb/geppetto/test/PartsOfAdultBrainTest.java
+++ src/test/java/uk/ac/vfb/geppetto/test/PartsOfAdultBrainTest.java
@@ -224,8 +224,8 @@ public class PartsOfAdultBrainTest
 		Assert.assertNotEquals("Definition", results2.getHeader().get(2));
 		Assert.assertEquals(1627, results2.getResults().size());
 		
-//		countOWL = owleryDataSource.getNumberOfResults(getRunnableQueries(model.getQueries().get(avQ.get("partsofOWL2")), variable));
-//		Assert.assertEquals(1627, countOWL);
+		countOWL = owleryDataSource.getNumberOfResults(getRunnableQueries(model.getQueries().get(avQ.get("partsofOWL2")), variable));
+		Assert.assertEquals(1627, countOWL);
 		
 //		results2 = owleryDataSource.execute(getRunnableQueries(model.getQueries().get(avQ.get("partsofOWL2")), variable));
 //		
