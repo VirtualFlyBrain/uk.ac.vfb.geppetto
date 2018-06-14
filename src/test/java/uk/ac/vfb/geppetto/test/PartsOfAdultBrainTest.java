f6c826aa0adcba66f08c2dd9c63f04f33fb5a84c
Rob Court <rcourt@ed.ac.uk>
2018-06-14 12:10:01 +0100

    readiing test
    partof is the compound AFAIK

--- src/test/java/uk/ac/vfb/geppetto/test/PartsOfAdultBrainTest.java
+++ src/test/java/uk/ac/vfb/geppetto/test/PartsOfAdultBrainTest.java
@@ -214,18 +214,18 @@ public class PartsOfAdultBrainTest
 		Assert.assertEquals("Images", results.getHeader().get(4));
 		Assert.assertEquals(1626, results.getResults().size());
 
-		int countOWL = owleryDataSource.getNumberOfResults(getRunnableQueries(model.getQueries().get(avQ.get("partsofOWL")), variable));
+		int countOWL = owleryDataSource.getNumberOfResults(getRunnableQueries(model.getQueries().get(avQ.get("partsof")), variable));
 		Assert.assertEquals(1627, countOWL);
 		
-		QueryResults results2 = owleryDataSource.execute(getRunnableQueries(model.getQueries().get(avQ.get("partsofOWL")), variable));
+		QueryResults results2 = owleryDataSource.execute(getRunnableQueries(model.getQueries().get(avQ.get("partsof")), variable));
 		
 		Assert.assertEquals("ID", results2.getHeader().get(0));
 		Assert.assertNotEquals("Name", results2.getHeader().get(1));
 		Assert.assertNotEquals("Definition", results2.getHeader().get(2));
 		Assert.assertEquals(1627, results2.getResults().size());
 		
-		countOWL = owleryDataSource.getNumberOfResults(getRunnableQueries(model.getQueries().get(avQ.get("partsofOWL2")), variable));
-		Assert.assertEquals(1627, countOWL);
+//		countOWL = owleryDataSource.getNumberOfResults(getRunnableQueries(model.getQueries().get(avQ.get("partsofOWL2")), variable));
+//		Assert.assertEquals(1627, countOWL);
 		
 //		results2 = owleryDataSource.execute(getRunnableQueries(model.getQueries().get(avQ.get("partsofOWL2")), variable));
 //		
