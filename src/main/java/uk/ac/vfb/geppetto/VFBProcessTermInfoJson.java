09de6d50009fb9a6a3b591234eb9d480a8b8d598
Rob Court <rcourt@ed.ac.uk>
2019-03-29 00:05:31 +0000

    import collections

--- src/main/java/uk/ac/vfb/geppetto/VFBProcessTermInfoJson.java
+++ src/main/java/uk/ac/vfb/geppetto/VFBProcessTermInfoJson.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Collections;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
