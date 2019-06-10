8252bd6d2f95ab2417a8338c9a9d185aa1a73b5f
Rob Court <rcourt@ed.ac.uk>
2019-06-10 14:44:05 +0100

    typo fix

--- src/main/java/uk/ac/vfb/geppetto/VFBProcessTermInfoJson.java
+++ src/main/java/uk/ac/vfb/geppetto/VFBProcessTermInfoJson.java
@@ -187,14 +187,14 @@ public class VFBProcessTermInfoJson extends AQueryProcessor
 
 		private String description() {
 			if (this.description != null && this.description.size() > 0) {
-				return "<span class=\"terminfo-description\">" + this.highlightLinks(String.join(" <br /> ", this.encode(this.description)) + "</span>";
+				return "<span class=\"terminfo-description\">" + this.highlightLinks(String.join(" <br /> ", this.encode(this.description))) + "</span>";
 			}
 			return "";
 		}
 
 		private String comment() {
 			if (this.comment != null && this.comment.size() > 0) {
-				return "<span class=\"terminfo-comment\">" + this.highlightLinks(String.join(" <br /> ", this.encode(this.comment)) + "</span>";
+				return "<span class=\"terminfo-comment\">" + this.highlightLinks(String.join(" <br /> ", this.encode(this.comment))) + "</span>";
 			}
 			return "";
 		}
