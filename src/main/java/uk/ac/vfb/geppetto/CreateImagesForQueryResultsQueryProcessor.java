24180849cf908dc991a846496d9ce28bb5a71263
Rob Court <rcourt@ed.ac.uk>
2019-07-01 16:45:53 +0100

    removing debug

--- src/main/java/uk/ac/vfb/geppetto/CreateImagesForQueryResultsQueryProcessor.java
+++ src/main/java/uk/ac/vfb/geppetto/CreateImagesForQueryResultsQueryProcessor.java
@@ -161,8 +161,8 @@ public class CreateImagesForQueryResultsQueryProcessor extends AQueryProcessor
 
 				i++;
 			}
-			System.out.println("CreateImagesForQueryResultsQueryProcessor returning " + Integer.toString(i) + " rows");
-			System.out.println(GeppettoSerializer.serializeToJSON(processedResults));
+			//System.out.println("CreateImagesForQueryResultsQueryProcessor returning " + Integer.toString(i) + " rows");
+			//System.out.println(GeppettoSerializer.serializeToJSON(processedResults));
 			return processedResults;
 		}
 		catch(GeppettoVisitingException e)
