444747424c95165389e25255215985059cb561ec
Rob Court <rcourt@ed.ac.uk>
2020-04-30 11:00:59 +0100

    adding debug

--- src/main/java/uk/ac/vfb/geppetto/VFBProcessTermInfoJson.java
+++ src/main/java/uk/ac/vfb/geppetto/VFBProcessTermInfoJson.java
@@ -1051,10 +1051,12 @@ public class VFBProcessTermInfoJson extends AQueryProcessor
 
 			try {
 				// checking the template
+				System.out.println("Checking template");
 				List<Type> templateTypes = (List<Type>) ModelUtility.getAllTypesOf("VFB_00101384");
 				System.out.println(templateTypes[0].getSuperType());
 				System.out.println(templateTypes);
 			} catch (Exception e) {
+				System.out.println("Error");
 				System.out.println(e);	
 			}
 			
