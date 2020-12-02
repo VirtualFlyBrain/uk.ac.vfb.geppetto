4278769104526abbbe1aab357222fed233384ea5
Rob Court <rcourt@ed.ac.uk>
2020-12-02 16:12:04 +0000

    Revert "using just first"
    This reverts commit da0010b445bd9cc6ca75d4376af5f42e7b49cee1.

--- src/main/java/uk/ac/vfb/geppetto/NEO4JQueryProcessor.java
+++ src/main/java/uk/ac/vfb/geppetto/NEO4JQueryProcessor.java
@@ -786,10 +786,10 @@ public class NEO4JQueryProcessor extends AQueryProcessor
 					if (hasDatasetCount) processedResult.getValues().add(String.format("%1$" + length + "s", row.dataset_counts.images.toString()));
 					if (hasExtra && row.extra_columns.size() > 0 && row.getScore() != null) processedResult.getValues().add(row.getScore());
 					if (hasSynCount){
-						processedResults.getValues().add(Float.toString(row.synapse_counts.downstream.get(0)));
-						processedResults.getValues().add(Float.toString(row.synapse_counts.Tbars.get(0)));
-						processedResults.getValues().add(Float.toString(row.synapse_counts.upstream.get(0)));
-						processedResults.getValues().add(Float.toString(row.synapse_counts.weight.get(0)));
+						processedResults.getValues().add(",".join(map(str, row.synapse_counts.downstream)));
+						processedResults.getValues().add(",".join(map(str, row.synapse_counts.Tbars)));
+						processedResults.getValues().add(",".join(map(str, row.synapse_counts.upstream)));
+						processedResults.getValues().add(",".join(map(str, row.synapse_counts.weight)));
 					}
 					processedResults.getResults().add(processedResult);
 				}catch (Exception e) {
