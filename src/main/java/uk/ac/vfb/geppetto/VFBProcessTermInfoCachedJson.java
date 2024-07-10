6969f5c5b9c04d7481443eba15e66bf74bbce888
Rob Court <rcourt@ed.ac.uk>
2024-07-10 17:53:32 +0100

    Flatterning muscle

--- src/main/java/uk/ac/vfb/geppetto/VFBProcessTermInfoCachedJson.java
+++ src/main/java/uk/ac/vfb/geppetto/VFBProcessTermInfoCachedJson.java
@@ -1730,7 +1730,7 @@ public class VFBProcessTermInfoCachedJson extends AQueryProcessor
 						}else if (runnableQuery.getPath().equals("TransgeneExpressionHere")) {
 							queryExpressedInX = "<a href=\"/org.geppetto.frontend/geppetto?q=" + variable.getId() + "," + (String) runnableQuery.getPath() + "\" data-instancepath=\"" + (String) runnableQuery.getPath() + "," + variable.getId() + "," + variable.getName() + "\">" + runnableQuery.getDescription().replace("$NAME", variable.getName()) + "</a></br>";
 						}
-					}else if ((superTypes.contains("Painted_domain") || superTypes.contains("Synaptic_neuropil_domain") || superTypes.contains("Synaptic_neuropil_subdomain") || superTypes.contains("Neuron_projection_bundle") || superTypes.contains("Split") || superTypes.contains("Expression_pattern")) && superTypes.contains("Individual") && classVariable.getId()!="notSet"){
+					}else if ((superTypes.contains("Painted_domain") || superTypes.contains("Synaptic_neuropil_domain") || superTypes.contains("Synaptic_neuropil_subdomain") || superTypes.contains("Neuron_projection_bundle") || superTypes.contains("Split") || superTypes.contains("Expression_pattern") || superTypes.contains("Muscle")) && superTypes.contains("Individual") && classVariable.getId()!="notSet"){
 						if(QueryChecker.check(runnableQuery, classVariable)){
 							querys += badge + "<a href=\"/org.geppetto.frontend/geppetto?q=" + classVariable.getId() + "," + (String) runnableQuery.getPath() + "\" data-instancepath=\"" + (String) runnableQuery.getPath() + "," + classVariable.getId() + "," + classVariable.getName() + "\">" + runnableQuery.getDescription().replace("$NAME", classVariable.getName()) + "</a></br>";
 						}
