ee1a8de0106298d40096c80d71461a5d4f476992
Rob Court <robbie.court@gmail.com>
2025-03-19 13:28:58 +0000

    enable debug mode in Neo4jToSOLRidQueryProcessor

--- src/main/java/uk/ac/vfb/geppetto/Neo4jToSOLRidQueryProcessor.java
+++ src/main/java/uk/ac/vfb/geppetto/Neo4jToSOLRidQueryProcessor.java
@@ -19,7 +19,7 @@ import org.geppetto.model.variables.Variable;
 public class Neo4jToSOLRidQueryProcessor extends AQueryProcessor {
 
     private Map<String, Object> processingOutputMap = new HashMap<>();
-    private Boolean debug = false;
+    private Boolean debug = true;
 
     @Override
     public QueryResults process(ProcessQuery query, DataSource dataSource, Variable variable, QueryResults results, GeppettoModelAccess geppettoModelAccess) throws GeppettoDataSourceException {
