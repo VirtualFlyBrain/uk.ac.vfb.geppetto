4ff5f5f209a49384a24087081cb63b2c61ad5a2f
Rob Court <robbie.court@gmail.com>
2025-03-25 19:14:42 +0000

    optimize garbage collection trigger in SOLRQueryProcessor to run every 50 processed results

--- src/main/java/uk/ac/vfb/geppetto/SOLRQueryProcessor.java
+++ src/main/java/uk/ac/vfb/geppetto/SOLRQueryProcessor.java
@@ -1010,10 +1010,9 @@ public class SOLRQueryProcessor extends AQueryProcessor
 				// Process in batches of 100 - write partial results to log
 				if ((i+1) % 10000 == 0) {
 					if (debug) System.out.println("Processed " + (i+1) + " of " + totalResults + " results");
-					// Force GC to run on batch completion (optional)
-					if (i > 100000) {
-						System.gc();
-					}
+				}
+				if ((i+1) % 100 == 0) {
+					System.gc();
 				}
 			}
 		  
